3a4e4bb8b881c8691fbf22a598f95ae73d89a5d1
dhatchinamoorthi <dhatchinamoorthi55@gmail.com>
2024-08-29 14:09:53 +0530

    Add .gitattributes to handle line endings

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
 import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
-import Contact from './pages/Contact';
 import './App.css';
 
 const App = () => (
@@ -17,7 +16,7 @@ const App = () => (
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
+       
       </Routes>
     </main>
   </Router>
