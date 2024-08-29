5fec4ed4643c23b25b63c74e18aaacd5b9ebea4c
dhatchinamoorthi <dhatchinamoorthi55@gmail.com>
2024-08-29 12:55:29 +0530

    Add .gitattributes for line endings management and update Sidebar and index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,12 @@ import reportWebVitals from './reportWebVitals';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 
+
+
+
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
