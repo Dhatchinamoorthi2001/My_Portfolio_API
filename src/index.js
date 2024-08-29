b3fc9e561de2de95981ddbe13770d891d2f1e0b6
dhatchinamoorthi <dhatchinamoorthi55@gmail.com>
2024-08-29 19:34:29 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -6,10 +6,19 @@ import reportWebVitals from './reportWebVitals';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 
+
+
+
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+
+
 reportWebVitals();
