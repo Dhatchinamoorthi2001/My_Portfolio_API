7513038eaa2c9ad2421069533cdb2d1783daf082
dhatchinamoorthi <dhatchinamoorthi55@gmail.com>
2024-08-30 14:44:31 +0530

    first commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ import './Home.css';
 const Home = () => (
   <div className="home-content">
     <header className="home-header">
-      <h1>HAY, I'M DHATCHINAMOORTHI</h1>
+      <h1>HEY, I'M DHATCHINAMOORTHI</h1>
       <p>Welcome to my portfolio! I am a Frontend Developer passionate about creating engaging and responsive web applications.</p>
       <Link to="/projects" className="cta-button">View My Projects</Link>
     </header>
